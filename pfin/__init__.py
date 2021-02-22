f644b031a27ce9349dd6d44ce5145b218f35168d
Lilian <lilian@boulard.fr>
2021-02-23 00:44:21 +0100

    Clean init

--- pfin/__init__.py
+++ pfin/__init__.py
@@ -1,13 +1,14 @@
 import sys
 import logging
 
+from .utils import *
+from .config import *
+from .user import User
 from .image import Image
 from .filter import Filter
-from .user import User, DummyUser
-from .config import PFIN_SERVER, PFIN_SECRET
 from .database import ImageDatabase, UserDatabase
 
-__all__ = ['Image', 'Filter', 'User', 'DummyUser', 'PFIN_SERVER', 'PFIN_SECRET', 'ImageDatabase', 'UserDatabase']
+__all__ = ['User', 'Image', 'Filter', 'ImageDatabase', 'UserDatabase']
 
 
 ###########
@@ -23,7 +24,7 @@ logger.setLevel(logging_level)
 formatter = logging.Formatter('%(asctime)s - [%(levelname)s] %(message)s')
 formatter.datefmt = '%m/%d/%Y %H:%M:%S'
 
-# fh = logging.FileHandler(filename='C:/Temp/qfap.log', mode='w')
+# fh = logging.FileHandler(filename='C:/Temp/pfin.log', mode='w')
 # fh.setLevel(logging_level)
 # h.setFormatter(formatter)
 
